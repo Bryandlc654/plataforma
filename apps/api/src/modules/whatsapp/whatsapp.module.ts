b15b764ac3060c767a68bd40191b8eee8ef2d369
Bryandlc654 <bdelacruz654@gmail.com>
2026-08-25 12:02:47 -0500

    fix(api): add ConfigModule to WhatsAppModule to prevent startup crash

--- apps/api/src/modules/whatsapp/whatsapp.module.ts
+++ apps/api/src/modules/whatsapp/whatsapp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { WhatsAppController } from "./whatsapp.controller";
 import { WhatsAppService } from "./whatsapp.service";
 
 @Module({
+  imports: [ConfigModule],
   controllers: [WhatsAppController],
   providers: [WhatsAppService],
   exports: [WhatsAppService],
